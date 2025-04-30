76e56ec11c86d0617ed28f86ef0b167d5e070683
usama <usamaliaqat678@gmail.com>
2025-04-29 23:59:58 -0700

    changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:holaa_tv/description_screen.dart';
 import 'package:holaa_tv/splash_screen.dart';
 
 
